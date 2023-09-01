c15e73049610f096b4ca2a0593235b91502521d9
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2023-09-01 08:14:51 +0100

    tweeting

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,8 @@ import Home from "./Components/Home";
 import Layer from "./Components/Layer";
 import Login from "./Components/Login";
 import ProfilePage from "./Components/ProfilePage";
-// import TweetFormState from "./Components/TweetFormState";
-// import TweetForm from "./Components/TweetForm";
 
 function App() {
-  // const { tweetFormClicked, closeTweetForm } = TweetFormState();
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
@@ -40,21 +37,6 @@ function App() {
   return (
     <>
       <RouterProvider router={router} />
-
-      {/* {tweetFormClicked ? (
-        <div className="tweet-form-modal absolute top-0 left-0 h-[100vh] w-[100vw]">
-          <div
-            className="overlay absolute top-0 right-0 left-0 bottom-0 bg-[#9f969643]"
-            onClick={closeTweetForm}
-          ></div>
-
-          <div className="bg-mainWhite w-[40%] h-[35%] mt-20 mx-auto rounded-2xl p-5 ">
-            <TweetForm cancelBtn={""} cancelBtnClick={closeTweetForm} />
-          </div>
-        </div>
-      ) : (
-        ""
-      )} */}
     </>
   );
 }
--- src/Components/TweetFormState.jsx
+++ src/Components/TweetFormState.jsx
@@ -2,20 +2,16 @@ import { useState } from "react";
 import React from "react";
 
 const TweetFormState = () => {
-  const [tweetFormClicked, setTweetFormClicked] = useState(true);
-  // to manage tweetForm opening and closing
+  const [tweetFormClicked, setTweetFormClicked] = useState(false);
 
   const openTweetForm = () => {
     setTweetFormClicked(true);
-  };
-  const testing = () => {
-    console.log("this is testing the tweetFormState us accessed");
   };
 
   const closeTweetForm = () => {
     setTweetFormClicked(false);
   };
-  return { tweetFormClicked, openTweetForm, closeTweetForm, testing };
+  return { tweetFormClicked, openTweetForm, closeTweetForm };
 };
 
 export default TweetFormState;
